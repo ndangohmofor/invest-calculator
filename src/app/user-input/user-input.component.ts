c873a1ef8a14cd68f5a19b82240a91002787c306
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-24 05:54:40 -0500

    create the initial binding in the user-input form

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,12 +1,19 @@
 import { Component } from '@angular/core';
+import { FormsModule } from '@angular/forms';
 
 @Component({
   selector: 'app-user-input',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './user-input.component.html',
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
-
+  initialInvestment = '0';
+  annualInvestment = '0';
+  duration = '0';
+  expectedReturn = '0';
+  onSubmit() {
+    console.log('Form submitted');
+  }
 }
