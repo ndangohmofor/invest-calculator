58cc2312e0afec4167f80e0934bc9fc9dc1bb2c0
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-24 06:11:13 -0500

    create the calculateresults fnction as an event

--- src/app/user-input/user-input.component.ts
+++ src/app/user-input/user-input.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, EventEmitter, Output } from '@angular/core';
 import { FormsModule } from '@angular/forms';
 
 @Component({
@@ -9,6 +9,7 @@ import { FormsModule } from '@angular/forms';
   styleUrl: './user-input.component.css'
 })
 export class UserInputComponent {
+  @Output() calculateResults = new EventEmitter();
   initialInvestment = '0';
   annualInvestment = '0';
   duration = '5';
