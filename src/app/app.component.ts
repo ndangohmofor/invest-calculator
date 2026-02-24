af75d5af56742172061dbbeb98b1ae12338c6180
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-24 06:06:17 -0500

    define the calculate results method in the app component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -9,4 +9,29 @@ import { UserInputComponent } from "./user-input/user-input.component";
   templateUrl: './app.component.html',
   imports: [HeaderComponent, UserInputComponent],
 })
-export class AppComponent {}
+export class AppComponent {
+  calculateInvestmentResults(data: {initialInvestment: number, duration: number, annualInvestment: number, expectedReturn: number}) {
+  const { initialInvestment, duration, annualInvestment, expectedReturn } = data;
+  const annualData = [];
+  let investmentValue = initialInvestment;
+
+  for (let i = 0; i < duration; i++) {
+    const year = i + 1;
+    const interestEarnedInYear = investmentValue * (expectedReturn / 100);
+    investmentValue += interestEarnedInYear + annualInvestment;
+    const totalInterest =
+      investmentValue - annualInvestment * year - initialInvestment;
+    annualData.push({
+      year: year,
+      interest: interestEarnedInYear,
+      valueEndOfYear: investmentValue,
+      annualInvestment: annualInvestment,
+      totalInterest: totalInterest,
+      totalAmountInvested: initialInvestment + annualInvestment * year,
+    });
+  }
+
+  return annualData;
+}
+
+}
