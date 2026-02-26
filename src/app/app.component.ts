020c2ac425fd26bce5a19c4223f820421dd93ab8
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-26 06:40:57 -0500

    specify the investment input input is a type

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import { Component } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import { IInvestmentInput } from './investment-input.model';
+import type { IInvestmentInput } from './investment-input.model';
 
 @Component({
   selector: 'app-root',
