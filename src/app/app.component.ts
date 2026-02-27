c51693684c48e108882fcfc6876ca35beb31f3c0
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-27 07:00:10 -0500

    update the app component to use the service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,7 @@
 import { Component, signal } from '@angular/core';
 import { HeaderComponent } from './header/header.component';
 import { UserInputComponent } from './user-input/user-input.component';
-import type {
-  IInvestmentInput,
-  IInvestmentResult,
-} from './investment-input.model';
+import type { IInvestmentResult } from './investment-input.model';
 import { InvestmentResultsComponent } from './investment-results/investment-results.component';
 
 @Component({
@@ -15,28 +12,4 @@ import { InvestmentResultsComponent } from './investment-results/investment-resu
 })
 export class AppComponent {
   resultsDate = signal<IInvestmentResult[] | undefined>(undefined);
-  onCalculateInvestmentResults(data: IInvestmentInput) {
-    const { initialInvestment, duration, annualInvestment, expectedReturn } =
-      data;
-    const annualData = [];
-    let investmentValue = initialInvestment;
-
-    for (let i = 0; i < duration; i++) {
-      const year = i + 1;
-      const interestEarnedInYear = investmentValue * (expectedReturn / 100);
-      investmentValue += interestEarnedInYear + annualInvestment;
-      const totalInterest =
-        investmentValue - annualInvestment * year - initialInvestment;
-      annualData.push({
-        year: year,
-        interest: interestEarnedInYear,
-        valueEndOfYear: investmentValue,
-        annualInvestment: annualInvestment,
-        totalInterest: totalInterest,
-        totalAmountInvested: initialInvestment + annualInvestment * year,
-      });
-    }
-
-    this.resultsDate.set(annualData);
-  }
 }
