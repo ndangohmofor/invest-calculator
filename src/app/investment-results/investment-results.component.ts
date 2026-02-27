6aee195a71bff82bb91241d2a2646197e69e4114
Ivo Mofor Ndangoh <ndangohmofor@gmail.com>
2026-02-27 05:44:54 -0500

    update the investment results to display currency and formatting using currency pipe

--- src/app/investment-results/investment-results.component.ts
+++ src/app/investment-results/investment-results.component.ts
@@ -1,10 +1,11 @@
 import { Component, Input, input } from '@angular/core';
 import type { IInvestmentResult } from '../investment-input.model';
+import { CurrencyPipe } from '@angular/common';
 
 @Component({
   selector: 'app-investment-results',
   standalone: true,
-  imports: [],
+  imports: [CurrencyPipe],
   templateUrl: './investment-results.component.html',
   styleUrl: './investment-results.component.css',
 })
